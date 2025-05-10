40baaad896274f56043295ef8a093d080150b3cf
kai <kaushikalex19@gmail.com>
2025-05-10 21:42:05 +0530

    remove element optimized two pointer method

--- practice/RemoveElement.java
+++ practice/RemoveElement.java
@@ -3,16 +3,13 @@ import java.util.List;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        List<Integer> tmp = new ArrayList<>();
-        for (int num : nums) {
-            if (num != val) {
-                tmp.add(num);
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
             }
         }
-        for (int i = 0; i < tmp.size(); i++) {
-            nums[i] = tmp.get(i);
-        }
-        return tmp.size();
+        return k;
     }
 
     public static void main(String[] args) {
